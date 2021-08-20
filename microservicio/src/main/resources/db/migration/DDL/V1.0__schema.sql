ef0d31abbaa922348535f67db6b093726feb1d9c
john.carrillo <john.carrillo@ceiba.com.co>
2021-08-20 16:22:06 -0500

    configuracion del docker file y del archivo de migraciones a la base de datos

--- microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -5,3 +5,27 @@ create table usuario (
  fecha_creacion datetime null,
  primary key (id)
 );
+
+create table contacto (
+ cont_id int(11) not null auto_increment,
+ cont_nombre varchar(100) not null,
+ cont_numero varchar(45) not null,
+ primary key (cont_id)
+);
+
+create table raza (
+ raza_id int(11) not null auto_increment,
+ raza_nombre varchar(100) not null,
+ primary key (raza_id)
+);
+
+create table mascota (
+ masc_id int(11) not null auto_increment,
+ masc_nombre varchar(100) not null,
+ raza_id int(11) not null,
+ masc_fecha_adopcion datetime null,
+ cont_id int(11) null,
+ primary key (masc_id),
+ constraint fk_mascota_raza foreign key(raza_id) references raza(raza_id),
+ constraint fk_mascota_contacto foreign key(cont_id) references contacto(cont_id)
+);
